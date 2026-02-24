1d458fc933bece90d7327180a1fd1291d8a30fd9
Shreyasi <shreyasi.kathait6@gmail.com>
2026-02-24 12:30:24 +0530

    otp trial3

--- routes/auth.js
+++ routes/auth.js
@@ -41,12 +41,14 @@ router.post("/send-otp", async (req, res) => {
     await Otp.create({ email: cleanEmail, otp, expiresAt });
 
     const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
